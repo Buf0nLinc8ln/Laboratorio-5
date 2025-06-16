39b014b4df016e7c43b5ccc43fed5681f042c764
Lincoln <davidveganaranjo08@gmail.com>
2025-06-16 00:35:03 -0600

    Implementacion de funciones

---
+++ src/ListasDoblementeEnlazadas.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/ListasDoblementeEnlazadas.h"
+
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+};
+
+
+struct Node *CrearNodo(int data){
+
+    struct Node* newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    return newNode;
+
+}
+
+void InsertarAlInicio(struct Node **head, int data) {
+    struct Node *newNode = CrearNodo(data);
+    newNode->next = *head;
+    if (*head != NULL) {
+        (*head)->prev = newNode;
+    }
+    *head = newNode;
+}   
+
+void InsertarAlFinal(struct Node **cola, int data) {
+    struct Node *newNode = CrearNodo(data);
+    if (*cola != NULL) {
+        newNode->prev = *cola;
+        (*cola)->next = newNode;
+    }
+} 
+
+void InsertarEnUnaPosicionEspecifica(struct Node *head, int data, int posicion) {
+    struct Node *newNode = CrearNodo(data);
+    int indice = 0;
+    struct Node *nodeActual = head;
+    while(nodeActual != NULL && indice < posicion) {
+        indice++;
+        nodeActual = nodeActual->next;
+    }
+        if (nodeActual != NULL) {
+            if (nodeActual->prev != NULL) {
+                newNode->prev = nodeActual->prev;
+                nodeActual->prev = newNode;
+                newNode->next = nodeActual;
+                nodeActual->prev->next = newNode;
+            }
+        } 
+      
+}
+
+void EliminarElemento(struct Node *head, int data) {
+    
+    struct Node *nodeActual = head;
+    while(nodeActual != NULL) {
+        if (nodeActual->data == data) {
+            if (nodeActual->prev != NULL) {
+                nodeActual->prev->next = nodeActual->next;
+            }
+            if (nodeActual->next != NULL) {
+                nodeActual->next->prev = nodeActual->prev;
+            }
+            free(nodeActual);
+            return;
+        }
+        nodeActual = nodeActual->next;
+    }
+
+}
+
+struct Node *BuscarElemento(struct Node *head, int data) {
+    
+    struct Node *nodeActual = head;
+    while(nodeActual != NULL) {
+        if (nodeActual->data == data) {
+            return nodeActual;
+        }
+        nodeActual = nodeActual->next;
+    }
+    return NULL;
+}
+
+
+void RecorrerHaciaAdelante(struct Node *head) {
+    
+    struct Node *nodeActual = head;
+    while(nodeActual != NULL) {
+        printf("%d ", nodeActual->data);
+        nodeActual = nodeActual->next;
+
+    }
+
+}
+
+void RecorrerHaciaAtras(struct Node *cola) {
+    
+    struct Node *nodeActual = cola;
+    while(nodeActual != NULL) {
+        printf("%d ", nodeActual->data);
+        nodeActual = nodeActual->prev;
+
+    }
+
+}
